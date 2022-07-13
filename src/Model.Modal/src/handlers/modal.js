74079f6545296f29fe8e3eb8e41018022631e1a5
devmarques7 <danimfgp@gmail.com>
2022-07-13 19:16:30 +0200

    Feature/ModalAtualizada

--- src/Model.Modal/src/handlers/modal.js
+++ src/Model.Modal/src/handlers/modal.js
@@ -6,6 +6,7 @@ const callModal = document.querySelectorAll('.callModal')
 
 callModal.forEach(button => {
     button.addEventListener('click', (event) => {
+        event.target.style.display = 'none'
         const typeOfButton = event.target.value
         const modalCreateHabit = creatStrutureHtmlModal(typeOfButton)
         bodyDocument.append(modalCreateHabit)
--- src/Model.Modal/src/modal/modalStruture.js
+++ src/Model.Modal/src/modal/modalStruture.js
@@ -4,7 +4,7 @@ export default function creatStrutureHtmlModal(typeOfModal) {
     switch (typeOfModal) {
         case 'creatHabit' :
             console.log(typeOfModal)
-            return createcreateHabitModal()
+            return createHabitModal()
         case 'editHabit'   :
             console.log(typeOfModal)
            return createEditHabitModal()
@@ -18,87 +18,178 @@ export default function creatStrutureHtmlModal(typeOfModal) {
 
 }
 
+// ELEMENTO PAI DO MODAL
+const bodyDocument = document.querySelector('body')
 
-function createcreateHabitModal(){
+// estrutura de modal Criar Habito
 
-    const formModal           = createHtmlElement('form', 'modal');
+function createHabitModal() {
 
+    // BLOCOS NECESSARIS PARA MODAL CRIAR HABITO 
+
+    // bloco container
+    const formModal           = createHtmlElement('form', 'modal creatHabit');
+
+    // bloco header
     const containerModalHeader = createHtmlElement('header', 'modal__header');
-
     const modalHeaderTitle       = createHtmlElement('h3', 'modal__title'); 
-
     const modalHeaderCloseButton   = createHtmlElement('img', 'modal__close');
 
     
-
+    // bloco caixas de ação
     const boxActionHabitTitle          = createActionBox('input','Título', 'title', 'Digitar título') 
-
     const boxActionHabitDescription      = createActionBox('textarea','Descrição', 'description', 'Digitar descrição') 
-
     const boxActionHabitSelectorCategory   = createActionSelectBox('Categoria', 'category')
+    const boxActionHabitButton               = createActionBoxButton('button','Inserir', 'submit', 'modal__submit')
 
-    const boxActionHabitButton               = createActionBoxButton('button', 'submit', 'modal__submit')
-
-
-
+    // Editando Elementos dentro do header 
     const modalHeaderCreated = appropriatingTagsHeader('Criar hábito', containerModalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal)
 
+
+    // implementando corpo de modal ao container 
     formModal.append(
-        containerModalHeader,
+        modalHeaderCreated,
         boxActionHabitTitle,
         boxActionHabitDescription,
         boxActionHabitSelectorCategory,
         boxActionHabitButton,
     ) 
-        
+      
+    // retornando modal completo
     return formModal
 }
 
-function createEditHabitModal() {
 
-    const formModal           = createHtmlElement('form', 'modal');
+function createEditHabitModal(){
 
-    const containerModalHeader = createHtmlElement('header', 'modal__header');
+    // BLOCOS NECESSARIS PARA MODAL EDITAR HABITO  
 
-    const modalHeaderTitle       = createHtmlElement('h3', 'modal__title'); 
+    // bloco container
+    const formModal                      = createHtmlElement('form', 'modal editHabit');
 
-    const modalHeaderCloseButton   = createHtmlElement('img', 'modal__close');
+    // bloco header
+    const containerModalHeader           = createHtmlElement('header', 'modal__header');
+    const modalHeaderTitle               = createHtmlElement('h3', 'modal__title'); 
+    const modalHeaderCloseButton         = createHtmlElement('img', 'modal__close');
 
     
+   // bloco caixas de ação
+    const boxActionHabitTitle            = createActionBox('input','Título', 'title', 'Tarefa a ser editada') 
 
-    const boxActionHabitTitle          = createActionBox('input','Título', 'title', 'Digitar título') 
+    const boxActionHabitDescription      = createActionBox('textarea','Descrição', 'description', 'Descrição a ser editada') 
+    const boxActionHabitSelectorCategory = createActionSelectBox('Categoria', 'category');
 
-    const boxActionHabitDescription      = createActionBox('textarea','Descrição', 'description', 'Digitar descrição') 
+    const containerActionCheckbox        = createHtmlElement('fieldset', 'container__action checkbox');
+    let containerActionTitleCheckbox     = createHtmlElement('label', 'title checkbox');
+    containerActionTitleCheckbox         = setInnerTextHtml(containerActionTitleCheckbox, 'Status')
+    const actionCheckbox                 = createActionBoxButton('input', null, 'checkbox', 'status__checkbox', '');
+    containerActionCheckbox.append(containerActionTitleCheckbox, actionCheckbox)
+    
+    const containerActionHabitButton     = createHtmlElement('div', 'container__box button');
+    const boxActionHabitButtonDelete     = createActionBoxButton('button','Excluir', 'checkbox', 'modal__submit delete', '')
+    const boxActionHabitButtonSave       = createActionBoxButton('button', 'Salvar', 'checkbox', 'modal__submit save', '')
+    containerActionHabitButton.append(boxActionHabitButtonDelete, boxActionHabitButtonSave)
+    
 
-    const boxActionHabitSelectorCategory   = createActionSelectBox('Categoria', 'category')
-
-    const boxActionHabitButton               = createActionBoxButton('button', 'submit', 'modal__submit')
-
+    // Editando Elementos dentro do header 
+    const modalHeaderCreated = appropriatingTagsHeader('Editar hábito', containerModalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal)
 
 
-    const modalHeaderCreated = appropriatingTagsHeader('Criar hábito', containerModalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal)
-
+    // implementando corpo de modal ao container 
     formModal.append(
-        containerModalHeader,
+        modalHeaderCreated,
         boxActionHabitTitle,
         boxActionHabitDescription,
         boxActionHabitSelectorCategory,
-        boxActionHabitButton,
+        containerActionCheckbox,
+        containerActionHabitButton
     ) 
         
+    // retornando modal completo
     return formModal
 }
+
+
 
 function createEditProfileModal() {
-    modalTagsContainer()
+
+    // BLOCOS NECESSARIS PARA MODAL EDITAR PROFILE 
+
+    // bloco container
+    const formModal                          = createHtmlElement('form', 'modalEditProfile');
+
+    // bloco header
+    const containerModalHeader               = createHtmlElement('header', 'modal__header');
+    const modalHeaderTitle                   = createHtmlElement('h3', 'modal__title'); 
+    const modalHeaderCloseButton             = createHtmlElement('img', 'modal__close');
+
+    
+   // bloco caixas de ação
+    const boxActionHabitTitle                = createActionBox('input','Título', 'title', 'Tarefa a ser editada') 
+
+    const boxActionHabitDescription          = createActionBox('textarea','URL da imagem do perfil', 'URLDaImagemDoPerfil', 'Nova foto de perfil', "tres") 
+
+    const createActionBoxButtonSalvarPerfil  = createActionBoxButton('button', 'Salvar alterações', 'submit', 'botaoSalvarPerfil')
+
+
+
+
+    // Editando Elementos dentro do header 
+    const modalHeaderCreated = appropriatingTagsHeader('Editar Perfil', containerModalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal)
+
+
+    // implementando corpo de modal ao container 
+    formModal.append(
+        modalHeaderCreated,
+        boxActionHabitTitle,
+        boxActionHabitDescription,
+        createActionBoxButtonSalvarPerfil
+    ) 
+        
+    // retornando modal completo
+    return formModal
 }
+
 
 function createDeleteModal() {
-    modalTagsContainer()
+
+    // BLOCOS NECESSARIS PARA MODAL DELETAR HABITO  
+
+    // bloco container
+    const formModal           = createHtmlElement('form', 'modal');
+
+    // bloco header
+    const containerModalHeader = createHtmlElement('header', 'modal__header');
+    const modalHeaderTitle       = createHtmlElement('h3', 'modal__title'); 
+    const modalHeaderCloseButton   = createHtmlElement('img', 'modal__close');
+
+    
+   // bloco caixas de ação
+    const boxActionHabitTitle          = createActionBox('input','Título', 'title', 'Tarefa a ser editada') 
+
+    const boxActionHabitDescription      = createActionBox('textarea','Descrição', 'description', 'Descrição a ser editada') 
+
+    const boxActionHabitSelectorCategory   = createActionSelectBox('Categoria', 'category');
+
+
+
+    // Editando Elementos dentro do header 
+    const modalHeaderCreated = appropriatingTagsHeader('Editar hábito', containerModalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal)
+
+
+    // implementando corpo de modal ao container 
+    formModal.append(
+        modalHeaderCreated,
+        boxActionHabitTitle,
+        boxActionHabitDescription,
+        boxActionHabitButton,
+    ) 
+        
+    // retornando modal completo
+    return formModal
 }
 
 
-const bodyDocument = document.querySelector('body')
 
 function createHtmlElement(element, classList){
     const tag = document.createElement(element);
@@ -108,6 +199,49 @@ function createHtmlElement(element, classList){
     return tag;
 }
 
+function createHeaderTitle(tagToSet, textToSet) {
+    const tag     = tagToSet 
+
+    tag.innerText = textToSet
+
+    return tag
+}
+
+function createActionBoxButton(element, placeholder, buttonType, classList, buttonValue) {
+
+    const button = createHtmlElement(element, classList)
+    button.type  = buttonType
+    button.name  = 'modalData'
+    buttonValue === ''? buttonValue : button.value = buttonValue
+    !placeholder? placeholder : button.innerText = placeholder;
+    
+    return button
+}
+
+
+
+function appropriatingTagsHeader(actionOfmodalTitle, modalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal) {
+
+    modalHeaderCloseButton = settingAtributesImg(modalHeaderCloseButton, './src/assets/X.png', 'close Icon', 'button')
+    modalHeaderCloseButton.addEventListener('click', () => {
+        bodyDocument.removeChild(formModal)
+    });
+
+    modalHeaderTitle = createHeaderTitle(modalHeaderTitle, actionOfmodalTitle)
+
+    modalHeader.append(modalHeaderTitle, modalHeaderCloseButton)
+
+    return modalHeader
+    
+}
+
+function setInnerTextHtml(tagToSet, textToSet) {
+    const tag     = tagToSet 
+
+    tag.innerText = textToSet
+
+    return tag
+}
 
 function settingAtributesTextActionBox(tagToSet, setAttributeId, setAttributePlaceholder) {
     const tag = tagToSet
@@ -131,68 +265,30 @@ function settingAtributesImg(tagToSet, setAttributeSrc, setAttributeAlt, setAttr
     return tag
 }
 
-function appropriatingTagsHeader(actionOfmodalTitle, modalHeader, modalHeaderTitle, modalHeaderCloseButton, formModal) {
 
-    modalHeaderCloseButton = settingAtributesImg(modalHeaderCloseButton, './src/assets/X.png', 'close Icon', 'button')
-    modalHeaderCloseButton.addEventListener('click', () => {
-        bodyDocument.removeChild(formModal)
-    });
 
-    modalHeaderTitle = createHeaderTitle(modalHeaderTitle, actionOfmodalTitle)
 
-    modalHeader.append(modalHeaderTitle, modalHeaderCloseButton)
 
-    return modalHeader
-    
-}
-
-function createHeaderTitle(tagToSet, textToSet) {
-    const tag     = tagToSet 
-
-    tag.innerText = textToSet
-
-    return tag
-}
-
-function setInnerTextHtml(tagToSet, textToSet) {
-    const tag     = tagToSet 
-
-    tag.innerText = textToSet
-
-    return tag
-}
-
-function createActionBox(textType, actionTypeTitle, classListActionType, placeholder) {
+function createActionBox(textType, actionTypeTitle, classListActionType, placeholder, identification) {
 
     let containerActionBox  = createHtmlElement('div', `box__actions ${classListActionType}`);
-    let actionBoxTitle      = createHtmlElement('span', 'title');
-    let labelActionBox      = createHtmlElement('label', classListActionType);
-    let textActionBox       = createHtmlElement(textType, `textArea ${classListActionType}`);  
+    let containerlabel      = createHtmlElement('label', `title ${classListActionType}`);
+    let textActionBox       = createHtmlElement(textType, `textArea ${classListActionType}`); 
     
-    actionBoxTitle     = setInnerTextHtml(actionBoxTitle, actionTypeTitle)
-    
-    labelActionBox.name = classListActionType
-    
+    textActionBox.id = identification
+
+    containerlabel     = setInnerTextHtml(containerlabel, actionTypeTitle)
+
+    containerlabel.append(textActionBox)
+
     textActionBox      = settingAtributesTextActionBox(textActionBox, classListActionType, placeholder)
 
-    containerActionBox.append(actionBoxTitle, labelActionBox, textActionBox)
+    containerActionBox.append(containerlabel)
 
     return containerActionBox
 }
 
-function appropriatingTagsOption(listOptionToCreate) {
 
-    const optionsCreated  = []
-
-    listOptionToCreate.forEach( option => {
-        const tag   = createHtmlElement('option', 'option.select');
-        tag.value    = option
-        tag.innerText = option
-        optionsCreated.push(tag)
-    });
-
-    return optionsCreated
-}
 
 function createActionSelectBox(actionTypeTitle, classListActionType) {
 
@@ -213,13 +309,18 @@ function createActionSelectBox(actionTypeTitle, classListActionType) {
 
 }
 
-function createActionBoxButton(element, buttonType, classList) {
-    const button = createHtmlElement(element, classList)
-    button.type  = buttonType
-    button.name  = 'modalData'
-    button.innerText = 'Inserir'
-    
-    return button
+function appropriatingTagsOption(listOptionToCreate) {
+
+    const optionsCreated  = []
+
+    listOptionToCreate.forEach( option => {
+        const tag   = createHtmlElement('option', 'option.select');
+        tag.value    = option
+        tag.innerText = option
+        optionsCreated.push(tag)
+    });
+
+    return optionsCreated
 }
 
 
